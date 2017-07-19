80677924976586a25dd0b49c2189390c0d21b0a5
Shaun Savage <savages@savages.com>
2017-07-19 13:50:10 +0000

    data loads and have ByIdx and ByKey working

--- src/components/VueData.vue
+++ src/components/VueData.vue
@@ -1,22 +1,128 @@
 <template>
   <div class="vue-data">
     <h1>{{ msg }}</h1>
+    <div class="hrow">
+      <div>
+        <select v-model="index">
+          <option v-for="n in range(0,9)">{{ n }}</option>
+        </select>
+      </div>
+      <div>
+        <select v-model="selectedKey">
+          <option v-for="k in keys">{{ k }}</option>
+        </select>
+      </div>
+      <div>
+        <button v-on:click="chkClick($event)">Check</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+import axios from 'axios'
+
 export default {
   name: 'VueData',
+
+  props: {
+
+  },
+
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      index: 5,
+      key: '',
+      selectedKey: null,
+      dataLoaded: []
     }
+  },
+
+  beforeCreate () {
+  },
+
+  created () {
+    this.loadData()
+  },
+
+  beforeMount () {
+
+  },
+
+  mounted () {
+  },
+
+  beforeUpdate () {
+
+  },
+
+  updated () {
+
+  },
+
+  methods: {
+    ...mapActions({
+      setNodes: 'setNodes'
+    }),
+
+    chkClick (evt) {
+      let itm = this.getNodeByKey(this.selectedKey)
+      console.log(Object.assign({}, itm))
+    },
+
+    loadData () {
+      let self = this
+      axios.get('http://10.0.42.81:8181/docs/local/budget/full')
+        .then(response => {
+          let rslt = response.data
+          let data = rslt.data
+          self.setNodes(data)
+          self.dataLoaded = []
+        })
+    },
+
+    range (min, max) {
+      let ary = []
+      for (let i = max; i >= min; i--) {
+        ary.push(i)
+      }
+      return ary
+    }
+
+  },
+
+  computed: {
+    ...mapGetters([
+      'nodes',
+      'rawData',
+      'getNodeByIdx',
+      'getNodeByKey'
+    ]),
+
+    keys: function () {
+      let ary = this.dataLoaded
+      for (let id in this.nodes) {
+        ary.push(id)
+      }
+      this.selectedKey = ary[0]
+      console.log('computed', ary)
+      return ary
+    }
+
+  },
+
+  watch: {
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hrow {
+  display:inline-block;
+}
 h1, h2 {
   font-weight: normal;
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,59 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createLogger from 'vuex/dist/logger'
 // import axios from 'axios'
 
 Vue.use(Vuex)
 
 import nodes from './modules/nodes'
 
-// const debug = process.env.NODE_ENV !== 'production'
+const debug = process.env.NODE_ENV !== 'production'
 
 const state = {
+  dataNodes: [],
+  nodes: {}
 }
 
 const getters = {
+  nodes: state => state.nodes,
+  rawData: state => state.dataNodes,
+  getNodeByIdx: state => (idx) => {
+    return state.dataNodes[idx]
+  },
+  getNodeByKey: state => (key) => {
+    return state.dataNodes[state.nodes[key].idx]
+  }
 }
 
 const mutations = {
+  SETNODES (state, nodes) {
+    if (state.dataNodes.length === 0) {
+      state.dataNodes = nodes
+      nodes.forEach(function (itm, idx) {
+        state.nodes[itm._id] = {idx: idx}
+      })
+    }
+  }
 }
 
 const actions = {
+  setNodes ({commit}, nodes) {
+    commit('SETNODES', nodes)
+  }
 }
 
 const modules = {
   nodes: nodes
 }
-// strict = debug
-//  plugins: debug ? [createLogger()] : []
+
+// const strict = debug
+// const plugins = debug ? [createLogger()] : []
+
 export default new Vuex.Store({
   state,
   getters,
   mutations,
   actions,
-  modules
+  modules,
+  plugins: debug ? [createLogger()] : []
 })
